badfadc35871c1dd7d7f42888c17056d50960fac
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-03-08 09:15:35 +0100

    Add files via upload

---
+++ Test 08032021 CSV/Test 08032021 CSV/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Test_08032021_CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string datoteka = File.ReadAllText("C:/Users/ivanm/Documents/Primjer.csv");
+            
+            Console.WriteLine(datoteka.Replace(",", " "));
+            File.Copy("C:/Users/ivanm/Documents/Primjer.csv", "C:/Users/ivanm/Documents/Primjer2.csv");
+            
+            Console.ReadKey();
+        }
+    }
+}
